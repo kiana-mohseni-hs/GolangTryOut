21ff4019db22ba748d875f9f60d8dc6b2030ef5f
KianaMohseni <kiana.mohseni@hootsuite.com>
2017-11-14 14:56:34 -0800

    Numerical print formats.

--- 02_numerals/main.go
+++ 02_numerals/main.go
@@ -4,10 +4,10 @@ import "fmt"
 
 func main() {
 
-	a := 0
+	a := 1000000
 	fmt.Println(42)
 	fmt.Printf("%d - %b - %x\n", 42, 42, 42)
-	for i := 0; i < 10; i++ {
+	for i := 1000000; i < 1000020; i++ {
 		fmt.Printf("%d - %b - %x\n", a , a, a)
 		a += 1
 	}
