d6cff99bc169414c7bfb399b264e17f7ecad3220
KianaMohseni <kiana.mohseni@hootsuite.com>
2017-11-14 16:32:22 -0800

    More on Packages
    More on Packages

--- 03_packages/main/main.go
+++ 03_packages/main/main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"fmt"
-	"github.com/GoesToEleven/GolangTraining/02_package/stringutil"
-	"github.com/GoesToEleven/GolangTraining/02_package/icomefromalaska"
+	"github.com/kiana-mohseni-hs/GolangTryOut/03_packages/stringutils"
+	"github.com/kiana-mohseni-hs/GolangTryOut/03_packages/icomefromalaska"
 	//someAlias "github.com/GoesToEleven/GolangTraining/02_package/icomefromalaska"
 )
 
 func main() {
-	fmt.Println(stringutil.Reverse("!oG ,olleH"))
-	fmt.Println(stringutil.MyName)
+	fmt.Println(stringutils.Reverse("!oG ,olleH"))
+	fmt.Println(stringutils.MyName)
 	fmt.Println(winniepooh.BearName)
 }
