66d2382589174100a9066beff19dab04fb94a630
KianaMohseni <kiana.mohseni@hootsuite.com>
2017-11-16 16:09:56 -0800

    Day 4

--- 12_if_else-if_else/08_divisibleByThree/main.go
+++ 12_if_else-if_else/08_divisibleByThree/main.go
@@ -2,10 +2,14 @@ package main
 
 import "fmt"
 
+var b func() = main
+
 func main() {
 	for i := 0; i <= 100; i++ {
 		if i%3 == 0 {
 			fmt.Println(i)
+			a := 3
+			fmt.Printf("%v", a)
 		}
 	}
 }
