eb5a2f66f21ef8a70c02217334735d7a7c2af26a
KianaMohseni <kiana.mohseni@hootsuite.com>
2017-11-22 18:32:06 -0800

    back at it. we can do this.

--- 19_map/14_hash-table/01_letter-buckets/01_runes-are-numbers/main.go
+++ 19_map/14_hash-table/01_letter-buckets/01_runes-are-numbers/main.go
@@ -6,4 +6,11 @@ func main() {
 	letter := 'A'
 	fmt.Println(letter)
 	fmt.Printf("%T \n", letter)
+
+	var this_is_a_rune rune
+	this_is_a_rune = 'K'
+	fmt.Println(this_is_a_rune)
+	fmt.Println( string(this_is_a_rune))
+	fmt.Println( rune(string(this_is_a_rune)[0]))
+
 }
